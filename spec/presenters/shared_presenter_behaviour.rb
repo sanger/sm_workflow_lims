486786480eb8d1d544f33a6ec1b026c1b4e82f9e
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2014-06-06 16:56:54 +0100

    Fixes test

--- spec/presenters/shared_presenter_behaviour.rb
+++ spec/presenters/shared_presenter_behaviour.rb
@@ -1,7 +1,7 @@
 shared_examples "shared presenter behaviour" do
   it "should yield each asset_type and its identifier in turn for each_asset_type" do
-    asset_type_1 = double("asset_type_1", :name=>'type1',:identifier_type=>'id1',:variable_samples=>true,:id=>1)
-    asset_type_2 = double("asset_type_2", :name=>'type2',:identifier_type=>'id2',:variable_samples=>false,:id=>2)
+    asset_type_1 = double("asset_type_1", :name=>'type1',:identifier_type=>'id1',:has_sample_count=>true,:id=>1)
+    asset_type_2 = double("asset_type_2", :name=>'type2',:identifier_type=>'id2',:has_sample_count=>false,:id=>2)
     AssetType.stub(:all) {[asset_type_1,asset_type_2]}
 
     expect { |b| presenter.each_asset_type(&b) }.to yield_successive_args(['type1', 'id1', true,1], ['type2', 'id2', false,2])
