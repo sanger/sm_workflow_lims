ebf32ed1380d10dec0824a058e43595de6133e6b
James Glover <james.glover@sanger.ac.uk>
2015-07-01 13:23:38 +0100

    Add methods to presenter

--- app/presenters/workflow/show.rb
+++ app/presenters/workflow/show.rb
@@ -4,8 +4,22 @@ module Presenter::WorkflowPresenter
 
   class Show < Presenter
 
+    attr_reader :workflow
+
     def initialize(workflow)
+      @workflow = workflow
     end
+
+    delegate :name, :has_comment, :reportable, :to => :workflow
+
+    def turn_around
+      workflow.turn_around_days
+    end
+
+    def action
+      "/admin/workflows/#{workflow.id}"
+    end
+
   end
 
 end
