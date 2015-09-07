0fc30e91f34e80de09acb646bbd2a0a1952a0ba3
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-09-07 15:53:20 +0100

    Tests for new days left or overdued, information labels

--- spec/presenters/asset_presenter_spec.rb
+++ spec/presenters/asset_presenter_spec.rb
@@ -61,14 +61,13 @@ describe Presenter::AssetPresenter::Asset do
   context "an unfinished asset" do
 
     let(:age) { date - DateTime.parse('01-02-2012 15:15') }
-    let(:asset) { double('asset',identifier:'asset_1',asset_type:mock_type,workflow:mock_workflow,study:'study',sample_count:2,begun_at:date,completed_at:nil,age:age) }
+    let(:asset) { double('asset',identifier:'asset_1',asset_type:mock_type,workflow:mock_workflow,study:'study',sample_count:2,begun_at:date,completed_at:nil,age:age, time_without_completion: 0) }
 
     include_examples "shared behaviour"
 
-    context 'when not due for a while' do
-      # DateTime returns differences in rationals. However, to avoid making assumptions about the performance of the standard
-      # library, we just use it. That way, if its behaviour changes, out tests will fail.
-      let(:age) { DateTime.parse('01-02-2012 15:15') - date }
+    context 'when no turn_around_days specified' do
+      let(:mock_workflow)  { double('mock_wf',  name:'Work',has_comment:true,turn_around_days:nil)}
+
       it "should return 'in progress' for completed_at" do
         presenter.completed_at.should eq('In progress')
       end
@@ -78,8 +77,24 @@ describe Presenter::AssetPresenter::Asset do
       end
     end
 
+    context 'when not due for a while' do
+      # DateTime returns differences in rationals. However, to avoid making assumptions about the performance of the standard
+      # library, we just use it. That way, if its behaviour changes, out tests will fail.
+      let(:age) { (DateTime.parse('01-02-2012 15:15') - date).to_i }
+
+      it "should return 'in progress' for completed_at" do
+        presenter.completed_at.should eq('In progress (2 days left)')
+      end
+
+      it "should have 'success' styling" do
+        expect(presenter.status_code).to eq('success')
+      end
+    end
+
     context 'when due today' do
-      let(:age) { DateTime.parse('03-02-2012 15:15') - date }
+      let(:today) { DateTime.parse('03-02-2012 15:15') }
+      let(:age) { today - date }
+      let(:asset) { double('asset',identifier:'asset_1',asset_type:mock_type,workflow:mock_workflow,study:'study',sample_count:2,begun_at:date,completed_at:nil,age:age, time_without_completion: age) }
       it "should return 'Due today' for completed_at" do
         presenter.completed_at.should eq('Due today')
       end
@@ -89,7 +104,9 @@ describe Presenter::AssetPresenter::Asset do
     end
 
     context 'when overdue' do
-      let(:age) { DateTime.parse('04-02-2012 15:15') - date }
+      let(:today) { DateTime.parse('04-02-2012 15:15') }
+      let(:asset) { double('asset',identifier:'asset_1',asset_type:mock_type,workflow:mock_workflow,study:'study',sample_count:2,begun_at:date,completed_at:nil,age:age, time_without_completion: age) }
+      let(:age) { today - date }
       it "should return 'Overdue (1 day)' for completed_at" do
         presenter.completed_at.should eq('Overdue (1 day)')
       end
@@ -100,11 +117,10 @@ describe Presenter::AssetPresenter::Asset do
   end
 
   context "an completed asset" do
-    let(:asset) { double('asset',identifier:'asset_1',asset_type:mock_type,workflow:mock_workflow,study:'study',sample_count:2,begun_at:date,completed_at:date) }
-
     include_examples "shared behaviour"
+    let(:asset) { double('asset',identifier:'asset_1',asset_type:mock_type,workflow:mock_workflow,study:'study',sample_count:2,begun_at:date,completed_at:date, time_without_completion: 0, age: 0) }
     it "should return its completed date for completed_at" do
-      presenter.completed_at.should eq('01/02/2012')
+      presenter.completed_at.should eq('01/02/2012 (Early 2 days)')
     end
   end
 
