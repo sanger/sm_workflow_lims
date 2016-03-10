47a48fd7fb5a9a1869c7ae0f35505eddb8e5eb37
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-03-10 14:43:28 +0000

    Weekdays will be evaluated taking into account the time of the date to calculate the number of ellapsed days (24 hours cycles) since that time

--- lib/weekdays.rb
+++ lib/weekdays.rb
@@ -1,43 +1,23 @@
 module Weekdays
-  def ellapsed_weekdays_before(day_of_week, number_of_days, options = {})
-    count_last_day = 0
-    count_last_day = 1 if options[:count_last_day]==true
-
-    ellapsed_weekdays=0
-    (count_last_day + number_of_days).times do |days|
-      actual_day_of_week =  ((day_of_week - days - 1 + count_last_day) % 7)
-      if (actual_day_of_week != 2) &&  (actual_day_of_week != 3)
-        ellapsed_weekdays +=1
-      end
-    end
-    ellapsed_weekdays
-  end
-
-
+  public
   def weekdays_between(date_a, date_b, options = {})
-    # Dates sorted to be correctly processed
-    unix_date_lower, unix_date_greater = [date_a, date_b].map(&:to_i).sort
-    # Number of days if there is more than 1 day in between
-    days = ((unix_date_greater - unix_date_lower) / (3600 * 24)).floor
-    # Any pass through 00:00 is considered a new day in between
-    days = 1 if days == 0 && date_a.day != date_b.day
-    # Complete weeks
-    weeks = (days / 7).floor
-    # Remainder days from the week counting
-    rest_of_days = (days % 7)
-    # Day of the week for the greater date of the range, from UNIX epoch
-    day_of_week = (unix_date_greater / (3600 * 24)) % 7
+    lower_date, greater_date = [date_a, date_b].sort
 
+    num_weeks = ((greater_date - lower_date).days / 1.week).floor
+    pivot_date = lower_date + (num_weeks * 1.week)
 
-    # Every week has 5 working days, plus the number of weekdays from the remainder
-    # day of the week
-    (weeks * 5) + ellapsed_weekdays_before(day_of_week, rest_of_days, options)
+    (num_weeks * 5) + num_weekdays_between(pivot_date, greater_date)
   end
-  private :ellapsed_weekdays_before
 
-  #def _weekdays_between(date_a, date_b)
-    #_weekdays_between(date_a, date_b, options = { :count_last_day => true })
-  #  weekdays_between(date_a, date_b)
-  #end
-  public :weekdays_between
+  private
+  def num_weekdays_between(lower_date, greater_date)
+    weekdays = 0
+    pivot_date = lower_date + 1.day
+    while (pivot_date <= greater_date) do
+      weekdays +=1 unless pivot_date.saturday? or pivot_date.sunday?
+      pivot_date += 1.day
+    end
+    weekdays
+  end
+
 end
--- spec/unit/weekdays_spec.rb
+++ spec/unit/weekdays_spec.rb
@@ -9,10 +9,10 @@ describe Weekdays do
 
     it "should identify the correct number of weekdays between two dates in the same week" do
       [
-        ['03-03-2016 13:15', '03-03-2016 13:15', 0],
+        ['03-03-2016 13:15', '03-03-2016 13:16', 0],
         ['03-03-2016 13:15', '04-03-2016 13:15', 1],
-        ['03-03-2016 13:15', '05-03-2016 13:15', 2],
-        ['03-03-2016 13:15', '06-03-2016 13:15', 2],
+        ['03-03-2016 13:15', '05-03-2016 13:15', 1],
+        ['03-03-2016 13:15', '06-03-2016 13:15', 1],
         ['03-03-2016 13:15', '07-03-2016 13:15', 2],
         ['03-03-2016 13:15', '08-03-2016 13:15', 3],
         ['03-03-2016 13:15', '09-03-2016 13:15', 4],
@@ -25,46 +25,35 @@ describe Weekdays do
 
     it "should identify the correct number of weekdays between dispersed dates" do
       [
-        ['03-03-2014 13:15', '03-03-2015 13:15', 261],    # Normal year
-        ['03-03-2015 13:15', '03-03-2016 13:15', 262],    # Leap year
-        ['01-10-1974 00:15', '15-08-2013 23:15', 10142],  # Far dates
+        ['03-03-2014 13:15', '03-03-2015 13:16', 260],    # Normal year
+        ['03-03-2015 13:15', '03-03-2016 13:16', 260],    # Leap year
+        ['01-10-1974 00:15', '15-08-2013 23:15', 10140],  # Far dates
         ['03-03-2016 13:15', '03-03-2016 13:14', 0],      # Same day, different time
         ['10-03-2016 13:15', '03-03-2016 13:14', 5],      # Reverse order
-        ['31-12-2014 23:59', '01-01-2015 00:00', 1],      # New year
+        ['31-12-2014 23:59', '01-01-2015 00:00', 0],      # New year
         ['05-03-2016 13:15', '06-03-2016 13:15', 0],      # Inside a weekend
         ['27-03-2016 00:00', '27-03-2016 23:59', 0],      # Change of time
         ['30-10-2016 00:00', '30-10-2016 23:59', 0],
-        ['27-03-2016 01:00', '28-03-2016 01:00', 0],
+        ['27-03-2016 00:00', '28-03-2016 00:00', 1],      # Change of time
+        ['30-10-2016 00:00', '31-10-2016 00:00', 1],      # Change of time
+        ['27-03-2016 01:00', '27-03-2016 23:59', 0],
+        ['27-03-2016 01:00', '28-03-2016 01:00', 1],
         ['30-10-2016 02:00', '31-10-2016 00:00', 0],
-        ['28-10-2016 13:15', '31-10-2016 13:15', 1],
-        ['25-03-2016 13:15', '28-03-2016 13:15', 1]
+        ['30-10-2016 02:00', '31-10-2016 02:00', 1],
+        ['28-10-2016 13:15', '31-10-2016 13:16', 1],
+        ['25-03-2016 13:15', '28-03-2016 13:16', 1]
       ].each do |date_a, date_b, weekdays|
         value = instance.weekdays_between(DateTime.parse(date_a), DateTime.parse(date_b))
         value.should eq(weekdays), "#{date_a} and #{date_b} should have #{weekdays} working days in between, but it was #{value}"
       end
     end
 
-    it "should count the correct number of weekdays if count last day is selected" do
-      [
-        ['03-03-2016 13:15', '03-03-2016 13:15', 1],
-        ['03-03-2016 13:15', '04-03-2016 13:15', 2],
-        ['03-03-2016 13:15', '05-03-2016 13:15', 2],
-        ['03-03-2016 13:15', '06-03-2016 13:15', 2],
-        ['03-03-2016 13:15', '07-03-2016 13:15', 3],
-        ['03-03-2016 13:15', '08-03-2016 13:15', 4],
-        ['03-03-2016 13:15', '09-03-2016 13:15', 5],
-        ['03-03-2016 13:15', '10-03-2016 13:15', 6],
-        ['01-10-1974 00:15', '15-08-2013 23:15', 10143],  # Far dates, and counting last day of range
-        ['28-10-2016 13:15', '31-10-2016 13:15', 2],      # Change of time
-        ['25-03-2016 13:15', '28-03-2016 13:15', 2],
-        ['27-03-2016 00:00', '27-03-2016 23:59', 0],      # Change of time and Sunday
-        ['30-10-2016 00:00', '30-10-2016 23:59', 0],      # Change of time and Sunday
-      ].each do |date_a, date_b, weekdays|
-        value = instance.weekdays_between(DateTime.parse(date_a), DateTime.parse(date_b), {:count_last_day => true})
-        value.should eq(weekdays), "#{date_a} and #{date_b} should have #{weekdays} working days in between, but it was #{value}"
-      end
+    it "should operate correctly with datetime days" do
+        date_a = DateTime.parse('03-03-2014 13:15')
+        date_b = date_a + 1.day
+        date_c = date_a + 7.day
+        instance.weekdays_between(date_a, date_b).should eq(1)
+        instance.weekdays_between(date_a, date_c).should eq(5)
     end
-
   end
-
 end
