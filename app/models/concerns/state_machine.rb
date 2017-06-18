c0c142a828e47a525bc34186556a4de8113c013d
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-06-18 20:36:32 +0100

    Changed some comments

--- app/models/concerns/state_machine.rb
+++ app/models/concerns/state_machine.rb
@@ -7,14 +7,14 @@ module StateMachine
     delegate :in_progress?, :volume_check?, :quant?, :report_required?, :reported?, to: :current_state
   end
 
-  # this method is used to move asset to the next state
+  # this method moves asset to the next state (like 'quant', 'report_required')
   def move_to_next(state)
     if !completed? || reportable?
       move_to(state)
     end
   end
 
-  # this method is used to mark asset as 'completed' or 'reported'
+  # this method marks asset as 'completed' or 'reported'
   def update(state)
     return unless state.present?
     move_to(state)
