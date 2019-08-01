8121b8747b4e24d1146e1ea597c6104076cf6734
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2019-08-01 15:33:11 +0100

    Fixed asset_type factory

--- spec/factories/asset_type_factories.rb
+++ spec/factories/asset_type_factories.rb
@@ -11,6 +11,7 @@ FactoryGirl.define do
   factory :asset_type do
     name { generate :asset_type_name }
     identifier_type { generate :asset_type_identifier }
+    labware_type { 'Plate' }
 
     factory :asset_type_has_sample_count do
       has_sample_count true
