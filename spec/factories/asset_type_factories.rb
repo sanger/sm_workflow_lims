962af4f53a6d8e327e512044d3fc73f4a382f09a
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2019-08-02 09:16:11 +0100

    Use factories, refactor asset_type_spec

--- spec/factories/asset_type_factories.rb
+++ spec/factories/asset_type_factories.rb
@@ -16,6 +16,18 @@ FactoryGirl.define do
     factory :asset_type_has_sample_count do
       has_sample_count true
     end
+
+    factory :asset_type_with_asset do
+      transient do
+        asset_identifier { 'test' }
+      end
+
+      after(:create) do |asset_type, evaluator|
+        create_list(:asset, 1,
+                    identifier: evaluator.asset_identifier,
+                    asset_type: asset_type)
+      end
+    end
   end
 
 end
