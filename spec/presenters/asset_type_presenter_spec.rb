f27c84461e32f8bdcacce4518cd5d1b342d3bde4
Harriet Craven <hc6@sanger.ac.uk>
2020-04-01 15:49:23 +0100

    Allow accepting of alphanumeric barcodes for Sample Management asset: Changed regex in asset type present to constant, to make easily testable Updated Sample management asset type identifier_data_type in config from numeric to alphanumeric Added default fields to asset type model spec Created asset type presenter spec testing validates_with method
    On deployment:
    Update the Sample management asset type manually in the console by running ..
    AssetType.find_by(name: 'Sample Management').update_attributes(identifier_data_type: 'alphanumeric')

---
+++ spec/presenters/asset_type_presenter_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Presenter::AssetTypePresenter::AssetType do
+
+  context '#validates_with' do
+    let(:asset_type) { create :asset_type, identifier_data_type: identifier_data_type }
+    let(:presenter) { Presenter::AssetTypePresenter::AssetType.new(asset_type) }
+
+    context 'when identifier_data_type is alphanumeric' do
+      let(:identifier_data_type) { 'alphanumeric' }
+
+      it 'returns the correct validation for the identifier data type' do
+        expect(presenter.validates_with).to eq Presenter::AssetTypePresenter::AssetType::ALPHANUMERIC_REGEX
+      end
+    end
+
+    context 'when identifier_data_type is numeric' do
+      let(:identifier_data_type) { 'numeric' }
+
+      it 'returns the correct validation for the identifier data type' do
+        expect(presenter.validates_with).to eq Presenter::AssetTypePresenter::AssetType::NUMERIC_REGEX
+      end
+    end
+  end
+end
