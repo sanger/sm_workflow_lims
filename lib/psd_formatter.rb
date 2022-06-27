59cb12af808fdd5499e113d9b9362bae52d00bf2
Katy Taylor <kt17@sanger.ac.uk>
2022-06-27 13:22:02 +0100

    rubocop. resolve in same way as in traction-service

--- lib/psd_formatter.rb
+++ lib/psd_formatter.rb
@@ -7,8 +7,8 @@ class PsdFormatter < Syslog::Logger::Formatter
   LINE_FORMAT = "(thread-%s) [%s] %5s -- : %s\n"
 
   def initialize(deployment_info)
-    info = OpenStruct.new(deployment_info)
-    @app_tag = [info.name, info.version, info.environment].compact.join(':').freeze
+    # deployment_info is set as DETAILS in deployed_version and called in deployment project
+    @app_tag = deployment_info.values.compact.join(':').freeze
     super()
   end
 
