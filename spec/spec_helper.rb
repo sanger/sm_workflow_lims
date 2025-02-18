3a3147ea838faceaf740d91b08630e2c48cf1df1
yoldas <ay6@sanger.ac.uk>
2025-02-18 23:39:35 +0000

    Switch to the new headless mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,11 +26,14 @@ require 'selenium/webdriver'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
+  options.add_argument('--disable_gpu')
+  options.add_argument('--window-size=1600,3200')
+  options.add_argument('--disable-search-engine-choice-screen')
 end
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless=old')
+  options.add_argument('--headless')
   options.add_argument('--disable_gpu')
   options.add_argument('--window-size=1600,3200')
   options.add_argument('--disable-search-engine-choice-screen')
