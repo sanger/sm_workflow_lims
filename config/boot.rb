1f71588ef82c592a06c56f93243bcc6546d0519d
yoldas <ay6@sanger.ac.uk>
2025-11-05 20:48:47 +0000

    boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+# Ensure stdlib logger is loaded before Rails or gems
+require 'rubygems'
+require 'logger'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
