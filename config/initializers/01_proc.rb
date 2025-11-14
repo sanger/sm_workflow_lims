4d0cf38d49182251912b71c8d59ce992a38b770e
Harriet Craven <harrietc52@hotmail.co.uk>
2025-11-14 11:39:05 +0000

    fix: rubocop

--- config/initializers/01_proc.rb
+++ config/initializers/01_proc.rb
@@ -1,9 +1,9 @@
 # www.mattsears.com/articles/2011/11/27ruby-blocks-as-dynamic-callbacks
 class Proc
-  def callback(callable, *args)
+  def callback(callable, *)
     self === Class.new do
       method_name = callable.to_sym
-      define_method(method_name) { |&block| block ? block.call(*args) : true }
+      define_method(method_name) { |&block| block ? block.call(*) : true }
       define_method(:"#{method_name}?") { true }
       def method_missing(*_args)
         false
